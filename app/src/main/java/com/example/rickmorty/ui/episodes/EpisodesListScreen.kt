14e4c101204d10932d1675b311faf63102f5edb4
Luis Felipe Vargas Quijada <luisfevq@gmail.com>
2026-03-03 21:50:44 -0300

    Listado principal

--- app/src/main/java/com/example/rickmorty/ui/episodes/EpisodesListScreen.kt
+++ app/src/main/java/com/example/rickmorty/ui/episodes/EpisodesListScreen.kt
@@ -1,10 +1,20 @@
 package com.example.rickmorty.ui.episodes
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.rickmorty.ui.fake.FakeEpisode
+import com.example.rickmorty.ui.fake.fakeEpisodes
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,9 +27,40 @@ fun EpisodesListScreen() {
         }
     ) { padding ->
 
-        Column(
-            modifier = Modifier.padding(padding)
-        ) { }
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            items(fakeEpisodes) { episode: FakeEpisode ->
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+
+                        }
+                ) {
+                    Column(
+                        modifier = Modifier.padding(12.dp)
+                    ) {
+                        Text(
+                            text = episode.name,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                        Text(
+                            text = "${episode.code} · ${episode.airData}",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+
+
+            }
+        }
 
     }
 }
